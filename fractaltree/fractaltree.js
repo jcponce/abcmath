cfedaa0ab9a95cadcdc98557db0b7bd34f3ad32c
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2018-06-26 22:11:14 +1000

    updated tree

--- fractaltree/fractaltree.js
+++ fractaltree/fractaltree.js
@@ -3,8 +3,8 @@ let aSlider, lSlider, rSlider;
 let checkbox;
 let r1, r2;
 
-let WIDTH = 1090;
-let HEIGHT = 736;
+let WIDTH = 810;
+let HEIGHT = 510;
 
 
 let angle1 = 0;
@@ -21,22 +21,23 @@ function setup() {
     controls();
 }
 
-//function animateTree() {
-//    if (this.checked()) { // if the selected check the tree is animated
-//        animate = true;
-//    } else {
-//        animate = false;
-//    }
-//}
+function animateTree() {
+    if (this.checked()) { // if the selected check the tree is animated
+        animate = true;
+    } else {
+        animate = false;
+    }
+}
 
 function draw() {
     background(0);
     
-    //noFill();
-    //stroke(100);
-    //rect(0,0,width-1, height-1);
-    translate(width / 2-150, height/2+250);
-    //rotate(radians(90));
+    translate(width / 2-90, height/2+210);
+   
+    textSize(18);
+    stroke(255);
+    fill(255);
+    text('PLAY', 400, 0);
     
     var Offset1 = 0;
     var Offset2 = 0;
@@ -64,12 +65,8 @@ function draw() {
         angle1 = 5;
         angle2 = 0;
         angle3 = 0;
-        tree(100, aSlider.value(), 0.1 * rSlider.value(), 0.1 * lSlider.value(), 10);
+        tree(70, aSlider.value(), 0.1 * rSlider.value(), 0.1 * lSlider.value(), 10);
     }
-    
-    translate(-width / 2+150, -height/2-250);
-    translate(WIDTH/2, HEIGHT/2);
-    buttons.forEach(button => button.update());
     
 }
 
@@ -78,9 +75,9 @@ function tree(length, ang, r1, r2, level) {
         strokeWeight(1);
         line(0, 0, 0, -length);
         //uncomment for leaves:
-        //translate(0,-length)
-        //fill(0,255,0);
-        //ellipse(0,0,15,30);
+        translate(0,-length)
+        fill(0,255,0);
+        ellipse(0,0,3,7);
     } else {
         push();
         stroke(100 - 10 * level, 100, 100);
@@ -98,73 +95,18 @@ function tree(length, ang, r1, r2, level) {
 function controls() {
     
     aSlider = createSlider(0, 80, 30, 0.01);
-    aSlider.position(880, 200);
-    aSlider.style('width', '160px');
+    aSlider.position(670, 200);
+    aSlider.style('width', '120px');
     lSlider = createSlider(0, 9, 6, 0.01);
-    lSlider.position(880, 280);
-    lSlider.style('width', '160px');
+    lSlider.position(670, 280);
+    lSlider.style('width', '120px');
     rSlider = createSlider(0, 9, 7, 0.01);
-    rSlider.position(880, 360);
-    rSlider.style('width', '160px');
+    rSlider.position(670, 360);
+    rSlider.style('width', '120px');
     
-    //checkbox = createCheckbox('.', false); // create a checkbox with a name and an initial value
-    //checkbox.position(780, 470); // where it's located
-    //checkbox.changed(animateTree); // what happens when it is checed
-    
-}
-
-
-class Button {
-    
-    constructor(x, y, width, height, text, onclick) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.text = text;
-        this.onclick = onclick;
-    }
-    
-    hovering() {
-        return mouseX > (this.x + WIDTH/2) - this.width/2 && mouseX < (this.x + WIDTH/2) + this.width/2 &&
-        mouseY > (this.y + HEIGHT/2) - this.height/2 && mouseY < (this.y + HEIGHT/2) + this.height/2;
-    }
-    
-    display() {
-        noStroke();
-        fill(0, 0, 40);
-        if (this.hovering())
-            fill(0, 0, 40);
-        rect(this.x - this.width/2, this.y - this.height/2, this.width, this.height);
-        fill(0, 0, 100);
-        textSize(this.height/2);
-        textAlign(CENTER, CENTER);
-        text(this.text, this.x, this.y);
-    }
-    
-    click() {
-        if (this.hovering()) {
-            this.onclick();
-        }
-    }
-    
-    update() {
-        this.display();
-    }
+    checkbox = createCheckbox(':', false); // create a checkbox with a name and an initial value
+    checkbox.position(690, 450); // where it's located
+    checkbox.changed(animateTree); // what happens when it is checed
     
 }
 
-function mousePressed() {
-    buttons.forEach(button => button.click());
-}
-
-var buttons = [
-               new Button(WIDTH/2 - WIDTH/8, -HEIGHT/2+600, WIDTH/14, HEIGHT/14, "Play", function() {
-                          animate = !animate;
-                          if (animate)
-                          this.text = "Stop";
-                          else
-                          this.text = "Play";
-                          }),
-               ];
-buttons.forEach(button => button.onclick = button.onclick.bind(button));
