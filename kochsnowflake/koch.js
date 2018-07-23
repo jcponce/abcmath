641845241641e08853c67e7af096489d67d59fc3
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2018-07-23 15:04:17 +1000

    koch

--- kochsnowflake/koch.js
+++ kochsnowflake/koch.js
@@ -15,8 +15,33 @@ var canvas;
 var startCol;
 var endCol;
 
+var sliderLayer;
+var sliderAngle;
+
+var sliderO;
+var sliderP;
+
+
 
 function setup() {
+    
+    var runningX = width * 0.25;
+
+    sliderLayer = createSlider(0, 4, 0, 1);
+    sliderLayer.position(runningX, height * 0.85);
+    sliderLayer.style('width', '180px');
+    
+    
+    sliderAngle = createSlider(2, 13, 3, 1);
+    sliderAngle.position(runningX, height * 2*0.85);
+    sliderAngle.style('width', '180px');
+    
+    sliderO = createSlider(-2, 2, -0.66, 0.01);
+    sliderO.position(runningX, height * 5*0.85);
+    sliderO.style('width', '180px');
+    sliderP = createSlider(0, 3.14, 1.57, 0.01);
+    sliderP.position(runningX, height * 6*0.85);
+    sliderP.style('width', '180px');
     
     var canvas = createCanvas(windowWidth, windowHeight);
     startCol = color("#ffffff");
@@ -28,16 +53,14 @@ function setup() {
         cosValues[i] = cos(radians(i));
     }
     initLines();
-    createGUI();
-    
     
 }
 
 function draw() {
     
-    
     initLines();
     background(0);
+    
     stroke(255);
     translate(width / 2, height / 2);
     for (var i = 0; i < lines.length; i++) {
@@ -45,6 +68,14 @@ function draw() {
     }
     translate(-width / 2, -height / 2);
     
+    textSize(20);
+    fill(255);
+    strokeWeight(1);
+    text('Step - ' + sliderLayer.value(), width*1/30, height * 3/30);
+    text('Angle -  ' + sliderAngle.value(), width*1/30, height * 7/30);
+    
+    text('Convex/Concave', width*1/30, height * 18/30);
+    text('Rotate', width*1/30, height * 22/30);
     
 }
 
@@ -57,14 +88,14 @@ function sm() {
 
 function initLines() {
     lines = [];
-    step = 360 / angle;
+    step = 360 / sliderAngle.value();
     end = createVector();
-    for (var i = 0; i < angle; i++) {
+    for (var i = 0; i < sliderAngle.value(); i++) {
         start = createVector(cosValues[(int(step * i) + 90) % 360] * r, sinValues[(int(step * i) + 90) % 360] * r);
         end = createVector(cosValues[(int(step * (i + 1)) + 90) % 360] * r, sinValues[(int(step * (i + 1)) + 90) % 360] * r);
         lines.push(new KochLine(end, start));
     }
-    for (var i = 0; i < layer; i++) {
+    for (var i = 0; i < sliderLayer.value(); i++) {
         generate();
     }
 }
@@ -102,9 +133,9 @@ var KochLine = function(_start, _end) {
     
     v.mult(0.5);
     this.kochC.add(v);
-    var s = map(width/3, 0, width, -2, 2);
+    var s = sliderO.value();//map(width/3, 0, width, -2, 2);
     v.mult(s);
-    var t = map(height/4, 0, height, 0, PI * 2);
+    var t = sliderP.value();//map(height/4, 0, height, 0, PI * 2);
     v.rotate(-t);
     this.kochC.add(v);
 };
@@ -123,95 +154,4 @@ KochLine.prototype.display = function() {
 
 function keyTyped() {
     save
-}
-
-
-function createGUI() {
-    var blank = 10;
-    var runningX = width * 0.05;
-    var p_back = "#4d170b";
-    var m_back = "#0b3e4d";
-    
-    var p_font_size = "18px"
-    var gui_w = "50px";
-    var gui_h = "30px";
-    
-    if (sm()) {
-        p_font_size = "12px"
-        gui_w = "50px";
-        gui_h = "20px";
-    }
-    
-    angle_p = createElement("span", "Angle: " + angle + " - ");
-    angle_p.style("color", "#fff");
-    angle_p.style("font-size", p_font_size);
-    angle_p.position(runningX, height * 0.85);
-    
-    a_plus_b = createButton("Plus");
-    a_plus_b.mousePressed(function() {
-                          if (angle < 15) angle += 1;
-                          angle_p.html("Angle: " + angle + " - ");
-                          });
-    a_plus_b.style("width", gui_w);
-    a_plus_b.style("height", gui_h);
-    a_plus_b.style("background", p_back);
-    a_plus_b.style("color", "#fff");
-    a_plus_b.style("border", "solid 2px #fff");
-    runningX += blank * 1 + angle_p.width;
-    a_plus_b.position(runningX, height * 0.85);
-    
-    a_minus_b = createButton("Minus");
-    a_minus_b.mousePressed(function() {
-                           if (angle > 2) angle -= 1;
-                           angle_p.html("Angle: " + angle + " - ");
-                           });
-    a_minus_b.style("width", gui_w);
-    a_minus_b.style("height", gui_h);
-    a_minus_b.style("background", m_back);
-    a_minus_b.style("color", "#fff");
-    a_minus_b.style("border", "solid 2px #fff");
-    runningX += blank + a_plus_b.width;
-    a_minus_b.position(runningX, (height * 0.85));
-    
-    runningX = width * 0.05;
-    var blankY = 10;
-    
-    layer_p = createElement("span", "Layer: " + layer + " - ");
-    layer_p.style("color", "#fff");
-    layer_p.style("font-size", p_font_size);
-    layer_p.position(runningX, height * 0.8 - blankY);
-    
-    l_plus_b = createButton("Plus");
-    l_plus_b.mousePressed(function() {
-                          var isOk = true;
-                          //if (layer == 5) isOk = confirm("This level is a very heavy processing.\n If you have not enough performance, you shoud be back to the previous level.")
-                          if (layer < 5) layer += 1;
-                          layer_p.html("Layer: " + layer + " - ");
-                          });
-    l_plus_b.style("width", gui_w);
-    l_plus_b.style("height", gui_h);
-    l_plus_b.style("background", p_back);
-    l_plus_b.style("color", "#fff");
-    l_plus_b.style("border", "solid 2px #fff");
-    runningX += blank * 1 + layer_p.width;
-    l_plus_b.position(runningX, height * 0.8 - blankY);
-    
-    l_minus_b = createButton("Minus");
-    l_minus_b.mousePressed(function() {
-                           if (layer > 0) layer -= 1
-                           layer_p.html("Layer: " + layer + " - ");
-                           });
-    l_minus_b.style("width", gui_w);
-    l_minus_b.style("height", gui_h);
-    l_minus_b.style("background", m_back);
-    l_minus_b.style("color", "#fff");
-    l_minus_b.style("border", "solid 2px #fff");
-    runningX += blank + l_plus_b.width;
-    l_minus_b.position(runningX, (height * 0.8 - blankY));
-    
-}
-
-
-function keyTyped() {
-    if(key=='s') saveCanvas("kochLine", "jpg");
 }
