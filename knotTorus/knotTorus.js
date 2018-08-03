f126fe5aa8f54d093e27d50d8599e32318c78fcb
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2018-08-03 20:52:15 +1000

    torus

--- knotTorus/knotTorus.js
+++ knotTorus/knotTorus.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * The p5.EasyCam library - Easy 3D CameraControl for p5.js and WEBGL.
  *
  *   Copyright 2018 by Thomas Diewald (https://www.thomasdiewald.com)
@@ -7,17 +7,19 @@
  *   Source: https://github.com/diwi/p5.EasyCam
  *
  *   MIT License: https://opensource.org/licenses/MIT
- * 
- * 
+ *
+ *
  * explanatory notes:
- * 
- * p5.EasyCam is a derivative of the original PeasyCam Library by Jonathan Feinberg 
+ *
+ * p5.EasyCam is a derivative of the original PeasyCam Library by Jonathan Feinberg
  * and combines new useful features with the great look and feel of its parent.
- * 
- * 
+ *
+ *
  */
 
 let easycam;
+
+
 
 // settings and presets
 let parDef = {
@@ -26,6 +28,7 @@ p: 2,
 q: 3,
 xyzAxes: axesSketch,
 Random: function() { this.p = floor(random(-40,40)); this.q = floor(random(-39,39)); },
+Torus: showTorus,
 };
 
 function setup() {
@@ -33,10 +36,11 @@ function setup() {
     // create gui (dat.gui)
     let gui = new dat.GUI();
     gui.add(parDef, 'Curve');
-    gui.add(parDef, 'p'  , -40, 40 ,1 ).listen();
-    gui.add(parDef, 'q'  , -39, 39 ,1 ).listen();
+    gui.add(parDef, 'p'  , -50, 50 ,1 ).listen();
+    gui.add(parDef, 'q'  , -49, 49 ,1 ).listen();
     gui.add(parDef, 'xyzAxes'  );
     gui.add(parDef, 'Random'  );
+    gui.add(parDef, 'Torus');
     //gui.add(this, 'backAttractors').name("Go Back");
     
     pixelDensity(1);
@@ -63,6 +67,13 @@ function axesSketch(){
     }else gizmo = false;
 }
 
+let showt = false;
+function showTorus(){
+    if(showt == false){
+        return showt = true;
+    }else showt = false;
+}
+
 function draw(){
     
     // projection
@@ -72,27 +83,34 @@ function draw(){
     background(0);
     
     noFill();
-    //stroke(255);
+    
     rotateX(0.9)
     rotateY(0.0);
     rotateZ(0.3);
     let hu = 50;
     beginShape();
-    for(let i = 0; i <= 2*PI; i+=PI/800){
+    for(let i = 0; i <= 2*PI; i+=PI/1000){
         stroke(hu, 255, 255);
-        strokeWeight(0.035);
-        let xc = 1.1*(cos(parDef.q*i)+2)*cos(parDef.p * i);
-        let yc = 1.1*(cos(parDef.q*i)+2)*sin(parDef.p * i);
-        let zc =  -1.1*sin(parDef.q * i);
+        strokeWeight(0.02);
+        let xc = 1*(cos(parDef.q*i)+2)*cos(parDef.p * i);
+        let yc = 1*(cos(parDef.q*i)+2)*sin(parDef.p * i);
+        let zc =  -1*sin(parDef.q * i);
         vertex(xc, yc, zc);
     }
     endShape(CLOSE);
     
+    if(showt){
+        stroke(0);
+        strokeWeight(0.015);
+        fill(102, 140, 255);
+        torus(2, 0.99);
+    }
+    
     if(gizmo==true){
-    // gizmo
-    strokeWeight(0.04);
-    stroke(255, 32,  0); line(0,0,0,1.5,0,0);
-    stroke( 32,255, 32); line(0,0,0,0,1.5,0);
-    stroke(  0, 32,255); line(0,0,0,0,0,1.5);
+        // gizmo
+        strokeWeight(0.04);
+        stroke(255, 32,  0); line(0,0,0,1.5,0,0);
+        stroke( 32,255, 32); line(0,0,0,0,1.5,0);
+        stroke(  0, 32,255); line(0,0,0,0,0,1.5);
     }
 }
