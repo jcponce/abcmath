4ea48d4e747ff97f85f3440b4a803c675ce48b8c
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2018-08-03 20:56:38 +1000

    lol

--- knotTorus/knotTorus.js
+++ knotTorus/knotTorus.js
@@ -50,7 +50,7 @@ function setup() {
     
     console.log(Dw.EasyCam.INFO);
     
-    easycam = new Dw.EasyCam(this._renderer, {distance : 9});
+    easycam = new Dw.EasyCam(this._renderer, {distance : 10});
     
     
 }
